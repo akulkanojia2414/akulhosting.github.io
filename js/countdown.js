756aa474e275e31e2d057147161d9d98d34d3222
akulhosting <akulkanojia.hosting@gmail.com>
2016-04-08 03:40:28 +0530

    Update countdown.js

--- js/countdown.js
+++ js/countdown.js
@@ -33,5 +33,5 @@ function initializeClock(id, endtime) {
   var timeinterval = setInterval(updateClock, 1000);
 }
 
-var deadline = new Date("June 15, 2016 12:00:00");
+var deadline = new Date("May 5, 2016 12:00:00");
 initializeClock('clockdiv', deadline);
